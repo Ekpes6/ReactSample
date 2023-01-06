f5f2f713b880a15e6293324ffc49b4c7371c0ec9
Ekpes6 <felixekpenyong6@gmail.com>
2023-01-06 21:12:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: true
+    };
+  }
+
+  render() {
+    return ( 
+      <div className="App">
+        <h1>
+          This is a Demo showing several ways to implement Conditional Rendering in React.
+        </h1>
+        <button>Login</button>
+        <button>Logout</button>
+      </div>
+    );
+  }
+}
+
+export default App;
